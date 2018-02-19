3ca94bf21ad6c3fb12a8c2314dc0aa1620f32bd0
Rob Ede <robjtede@icloud.com>
2018-02-19 19:42:11 +0000

    add todo list

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements OnInit {
+  todos = [
+    {text: 'explain angular', done: true},
+    {text: 'write program', done: false},
+    {text: 'go to the cavendish', done: false}
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  markAs(todo, done) {
+    this.todos.find((x) => x === todo).done = done;
+  }
+
+}
